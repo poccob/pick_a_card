c57d73d78b86561f67003c7227f32bd46ce1e211
poccob <poccob@mail.ru>
2017-09-25 11:25:15 +0800

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,15 @@
 # encoding: UTF-8
-values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
 
-puts "#{values.sample} of #{suits.sample}"
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+suits = ["♦", "♠", "♥ ", "♣"]
+
+puts "#{values.sample}#{suits.sample}"
